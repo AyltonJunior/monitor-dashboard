016ccbbb29fba1fbf38e6d9d5450211150773320
Aylton Junior <grupohisistemas@gmail.com>
2025-01-22 16:30:26 -0300

    fix: atualiza configurações do Vite e adiciona versão do Node

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: process.cwd(),
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   server: {
-    host: true, // Necessário para conexões de rede
-    port: 5173, // Porta padrão do Vite
-    strictPort: true, // Não tentar outras portas se 5173 estiver em uso
+    host: true,
+    port: 8080,
+    strictPort: true,
     hmr: {
-      clientPort: 5173 // Força o cliente a usar a porta 5173 para HMR
+      clientPort: 8080
     }
   }
 })
