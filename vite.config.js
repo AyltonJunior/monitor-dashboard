5c14d9f14031331231c914b4d30cfb06320c7bf4
Aylton Junior <grupohisistemas@gmail.com>
2025-01-22 16:36:08 -0300

    fix: adiciona configurações explícitas de build e Procfile

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 8080
