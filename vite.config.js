93bdace928827cf94529bebe7bb8881dfa5ffb7d
Aylton Junior <grupohisistemas@gmail.com>
2025-01-22 17:05:50 -0300

    chore: remove configurações do DigitalOcean e simplifica projeto

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
-  },
   server: {
-    host: '0.0.0.0',
-    port: 8080
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 8080
+    port: 3000
   }
 })
