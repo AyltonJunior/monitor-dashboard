c17043534e0bb57154617b4173114df2ad0dcd2b
Aylton Junior <grupohisistemas@gmail.com>
2025-01-22 16:33:32 -0300

    fix: simplifica configuração do Vite e corrige versão do Node

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: process.cwd(),
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
   server: {
-    host: true,
-    port: 8080,
-    strictPort: true,
-    hmr: {
-      clientPort: 8080
-    }
+    host: '0.0.0.0',
+    port: 8080
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8080
   }
 })
